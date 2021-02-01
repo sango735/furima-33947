fb17b63cfd06a4a6d2d9879eceeced3fb1d96c82
sango735 <735th.floor@gmail.com>
2021-02-01 18:59:36 +0900

    バリデーションの微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,6 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
     validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/ }
     validates :nickname
-    validates :birthday
+    validates :birthday  
   end
 end
