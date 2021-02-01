2a576ab0c66c4e92c187268b08e78efb28da3903
sango735 <735th.floor@gmail.com>
2021-02-01 17:43:16 +0900

    単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :first_name,       presence: true
-  validates :last_name,        presence: true
-  validates :first_name_kana,  presence: true
-  validates :last_name_kana,   presence: true
   validates :birthday,         presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
