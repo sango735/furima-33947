6ff4fdaa04290f9ddaeaa0176f436b34dcf57968
sango735 <735th.floor@gmail.com>
2021-02-01 18:01:28 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname,         presence: true
   validates :birthday,         presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :first_name
     validates :last_name
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 6)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname { Faker::Name.initials(number: 6) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
 
     gimei = Gimei.name
-    first_name            {gimei.first.kanji}
-    last_name             {gimei.last.kanji}
-    first_name_kana       {gimei.first.katakana}
-    last_name_kana        {gimei.last.katakana}
-    birthday              {Faker::Date.birthday}
-   end
+    first_name            { gimei.first.kanji }
+    last_name             { gimei.last.kanji }
+    first_name_kana       { gimei.first.katakana }
+    last_name_kana        { gimei.last.katakana }
+    birthday              { Faker::Date.birthday }
+  end
 end
