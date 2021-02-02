5d964b249609f89beb48bcab69755e798114ea76
sango735 <735th.floor@gmail.com>
2021-02-02 14:43:03 +0900

    ActiveHashの導入と設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :burden_id, :state_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,20 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :category_id
-    validates :state_id
-    validates :burden_id
-    validates :area_id
-    validates :days_id
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :state_id, numericality: { other_than: 1 } 
+    validates :burden_id, numericality: { other_than: 1 } 
+    validates :area_id, numericality: { other_than: 1 } 
+    validates :days_id, numericality: { other_than: 1 } 
     validates :price
+    validates :image
   end
 
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :burden
+  belongs_to :category
+  belongs_to :days
+  belongs_to :state
 end
