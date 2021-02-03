b2b3ddac03dff32674855ba66f69ec9bbdef8b4b
sango735 <735th.floor@gmail.com>
2021-02-03 17:33:38 +0900

    指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :redirect_index, only: [:edit, :update]
   before_action :item_find_id, only: [:show, :edit, :update, :redirect_index]
+  before_action :redirect_index, only: [:edit, :update]
 
   def index
     @item = Item.includes(:user).order(id: 'DESC')
@@ -40,15 +40,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :explanation, :category_id, :burden_id, :state_id, :area_id, :days_id,
                                  :price).merge(user_id: current_user.id)
   end
-
-  def redirect_index
+  def item_find_id
     @item = Item.find(params[:id])
+  end
+  def redirect_index
       unless current_user.id == @item.user_id
         redirect_to root_path
       end
   end
 
-  def item_find_id
-    @item = Item.find(params[:id])
-  end
 end
