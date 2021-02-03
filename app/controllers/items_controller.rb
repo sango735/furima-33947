24e491ad76325caebd051a887f2659c0aa78199c
sango735 <735th.floor@gmail.com>
2021-02-03 16:27:38 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :redirect_index, only: [:edit, :update]
-  before_action :item_find_id, only: [:show, :edit, :update,:redirect_index]
+  before_action :item_find_id, only: [:show, :edit, :update, :redirect_index]
 
   def index
     @item = Item.includes(:user).order(id: 'DESC')
@@ -22,6 +22,7 @@ class ItemsController < ApplicationController
 
   def show
   end
+
   def edit
   end
 
@@ -39,12 +40,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :explanation, :category_id, :burden_id, :state_id, :area_id, :days_id,
                                  :price).merge(user_id: current_user.id)
   end
+
   def redirect_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
   end
+
   def item_find_id
     @item = Item.find(params[:id])
   end
