909032987d2ed929e20f90c26e8c064f1e7a1f97
sango735 <735th.floor@gmail.com>
2021-02-03 16:26:08 +0900

    出品者以外の編集画面遷移の制限実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :redirect_index, only: [:edit, :update]
+  before_action :item_find_id, only: [:show, :edit, :update,:redirect_index]
 
   def index
     @item = Item.includes(:user).order(id: 'DESC')
@@ -19,14 +21,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.user_id)
     else
@@ -39,5 +38,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :burden_id, :state_id, :area_id, :days_id,
                                  :price).merge(user_id: current_user.id)
+  end
+  def redirect_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+  def item_find_id
+    @item = Item.find(params[:id])
   end
 end
