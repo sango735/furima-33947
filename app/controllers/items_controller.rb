3c32e257b84fd58988cc94ceb93d92854a98b34c
sango735 <735th.floor@gmail.com>
2021-02-03 14:11:57 +0900

    商品並び順変更（新しい順）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @item = Item.includes(:user)
+    @item = Item.includes(:user).order(id: "DESC")
   end
 
   def new
