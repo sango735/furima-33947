2c11e86744f736f1bae1be399547a6e8a7a7e5cc
sango735 <735th.floor@gmail.com>
2021-02-05 18:55:46 +0900

    バリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,5 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
+  attr_accessor :token, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number
 
-  
+  with_options presence: true do
+   validates :token
+   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
+   validates :area_id, numericality: { other_than: 1, message: 'Select' }
+   validates :municipality
+   validates :address
+   validates :phone_number, numericality: { only_integer: true }
+  end
+
 end
