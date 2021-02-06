9ea4a8020675c800e87b3836c498b8e3e3f35f1c
sango735 <735th.floor@gmail.com>
2021-02-06 16:11:13 +0900

    出品者が購入ページへ遷移した時の画面遷移設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,18 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :redirect_index, only: [:index, :create]
+
 
   def index
     @item = Item.find(params[:item_id])
   end
   def create
   end
+
+  private
+  def redirect_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
 end
