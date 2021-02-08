8020a428d1f624bcd6c7e137494301c73bb4fa2c
sango735 <735th.floor@gmail.com>
2021-02-08 16:38:00 +0900

    テストコードの実装

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    token           {"tok_abcdefghijk00000000000000000"}
+    postal_code     { "123-4567" }
+    area_id         { Faker::Number.between(from: 2, to: 3) }
+    municipality    { "横浜市緑区" }
+    town            { "123" }
+    building_name   { "test" }
+    phone_number    { "09012345678" }
+  end
+end
